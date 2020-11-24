2a7dd8fcc069a35d406b88f5ce0bd8ffdad06e8d
Ike <ikescode@gmail.com>
2020-11-24 15:59:31 -0600

    add audio visualization, audio element, logo and styles

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import AudioSpectrum from 'react-audio-spectrum';
+
 
 export default function Home() {
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Sunny97-7 Unseasonably Sunny [LIVE]</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main>
+        <div className="player">
+          <img
+              className="player__img"
+              src="sunny-logo.png"
+            />
+          <audio 
+            className="player__audio"
+            id="audio-element"
+            src="http://157.245.253.163:8000/unseasonably-sunny"
+            autoPlay
+            controls
+            crossOrigin="anonymous"
+          />
+            <AudioSpectrum
+              className="player__AudioSpectrum"
+              id="audio-canvas"
+              height={200}
+              width={300}
+              audioId={'audio-element'}
+              capColor={'orange'}
+              capHeight={1}
+              meterWidth={2}
+              meterCount={80}
+              meterColor={[
+                {stop: 0, color: '#FB3B44'},
+                {stop: 0.5, color: '#FFEB13'},
+                {stop: 1, color: 'red'}
+              ]}
+              gap={4}
+            />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
