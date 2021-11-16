665b75609cd54a88b731606b3037efbd76dbc2a9
Ike <ikescode@gmail.com>
2021-11-16 14:43:44 -0600

    netlify to vercel

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Sunny97-7 Unseasonably Sunny [LIVE]</title>
+        <title>Sunny 97.7 Unseasonably Sunny [LIVE]</title>
       </Head>
 
       <main>
